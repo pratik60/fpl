ca06756cf15942cbd054c2a8265be88aee7e3933
Pratik Bothra <pratik60@gmail.com>
2017-04-16 21:17:49 +0100

    Initializers

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -13,5 +13,31 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it 'responds with 200 status code' do
       expect(response.code).to eq('200')
     end
+
+    it 'returns the serialized user attributes' do
+      expect(JSON.parse(response.body)['data']['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    end
+
   end
+
+  describe 'GET #index' do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+      auth_headers = @user.create_new_auth_token
+      request.headers.merge!(auth_headers)
+      get :index, id: @user.id
+    end
+
+    it 'responds with 200 status code' do
+      expect(response.code).to eq('200')
+    end
+
+    it 'returns the serialized user attributes' do
+      expect(JSON.parse(response.body)['data'].length).to eq(1)
+      expect(JSON.parse(response.body)['data'].first['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    end
+
+  end
+
 end
