4eebb04019b1519ac68a88903eee615e8a3a01b1
Pratik Bothra <pratik60@gmail.com>
2017-04-17 20:43:09 +0100

    Double quotes

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  describe 'GET #show' do
+  describe "GET #show" do
 
     before(:each) do
       @user = FactoryGirl.create :user
@@ -10,17 +10,17 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       get :show, id: @user.id
     end
 
-    it 'responds with 200 status code' do
-      expect(response.code).to eq('200')
+    it "responds with 200 status code" do
+      expect(response.code).to eq("200")
     end
 
-    it 'returns the serialized user attributes' do
-      expect(JSON.parse(response.body)['data']['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    it "returns the serialized user attributes" do
+      expect(JSON.parse(response.body)["data"]["attributes"]).to eq({"name"=>"John Doe", "email"=>"test@test.com"})
     end
 
   end
 
-  describe 'GET #index' do
+  describe "GET #index" do
 
     before(:each) do
       @user = FactoryGirl.create :user
@@ -29,13 +29,13 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       get :index, id: @user.id
     end
 
-    it 'responds with 200 status code' do
-      expect(response.code).to eq('200')
+    it "responds with 200 status code" do
+      expect(response.code).to eq("200")
     end
 
-    it 'returns the serialized user attributes' do
-      expect(JSON.parse(response.body)['data'].length).to eq(1)
-      expect(JSON.parse(response.body)['data'].first['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    it "returns the serialized user attributes" do
+      expect(JSON.parse(response.body)["data"].length).to eq(1)
+      expect(JSON.parse(response.body)["data"].first["attributes"]).to eq({"name"=>"John Doe", "email"=>"test@test.com"})
     end
 
   end
